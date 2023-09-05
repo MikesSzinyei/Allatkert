7f36bc506f557e65e14e8dad2f66047e642a1407
Mikes Szinyei <szinyeimikes@gmail.com>
2023-09-05 11:35:50 +0200

    kezdés

---
+++ README.md
@@ -0,0 +1,5 @@
+# Állatkert
+
+Állatok szilmulációja
+
+OOP programozással ismerkedés.
---
+++ src/Allat.java
@@ -0,0 +1,20 @@
+public class Allat  {
+    String nev="nevtelen";
+
+    public void beallitnev(String nev) {
+        if(nev.isEmpty()) {
+            System.out.println("Hiba! nem adtál meg nevet");
+            System.exit(1);
+        }
+        this.nev=nev;
+        
+    }
+
+    public String lekerNev() {
+        return this.nev;
+    }
+
+    public void hangotad(String hang) {
+        System.out.println(hang);
+    }
+}
